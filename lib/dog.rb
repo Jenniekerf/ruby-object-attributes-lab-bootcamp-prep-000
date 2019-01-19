2760d5098b2ff64b3b743dedc26b0cefbbc49d8c
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-01-19 17:43:32 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog 
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def name
+    @name = name
+  end
+  
+end
+  
+  def breed 
+    @breed = breed 
+  end
+  
+  def breed=(new_breed)
+    @breed = new_breed
+  end 
+  
+end 
